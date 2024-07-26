77a4613fff872079e0ea42357c3d3e986fa41539
Sanjay PS <39940357+Sanjay-PS@users.noreply.github.com>
2024-07-27 00:16:47 +0530

    Boilerplate version

---
+++ README.md
@@ -0,0 +1,4 @@
+# To do
+
+A simple to-do app using react
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div>
+            <h1>
+                <center>
+                    Let's starting build the to-do app together!
+                </center>
+            </h1>
+        </div>
+    )
+}
+
+export default App
